61b59f33b0b51a9055cc23e19ece7a58e6583096
Luis Gerardo <luis.encinas1506@gmail.com>
2022-03-03 18:11:28 -0800

    Se agrego animation

--- public/js/main.js
+++ public/js/main.js
@@ -41,6 +41,22 @@ $(window).on("load", function () {
       }
     }
   });
+  $('.owl-carousel-animation').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 5
+      }
+    }
+  });
   /*
   $('.carousel-wrapper .right').click(function() {
     owl.trigger('next.owl.carousel');
--- resources/views/animation.blade.php
+++ resources/views/animation.blade.php
@@ -14,106 +14,35 @@
             <span><a href="{{ url('/contact') }}"> Contact</a></span>
           </div>
       </div>
-      <div class="design-first-section">
-         <div class="row">
-            <div class="col-12 col-md-5 design-first-section-col-1">
-               <div class="design-first-section-text-wrapper">
-                  <p class="design-first-section-text-1">DESIGN</p>
-                  <p class="design-first-section-text-2">PHRASE HERE</p>
-                  <p class="design-first-section-text-3">Algo más por aqui</p>
-               </div>
-            </div>
-            <div class="col-12 col-md-7 design-first-section-second-col">
-               <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-                  <div class="carousel-inner">
-                     <div class="carousel-item active">
-                        <img src="https://cdn.quasar.dev/img/mountains.jpg" class="d-block w-100" alt="...">
-                     </div>
-                     <div class="carousel-item">
-                        <img src="https://cdn.quasar.dev/img/parallax1.jpg" class="d-block w-100" alt="...">
-                     </div>
-                     <div class="carousel-item">
-                        <img src="https://cdn.quasar.dev/img/parallax2.jpg" class="d-block w-100" alt="...">
-                     </div>
-                  </div>
-                  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                  </button>
-                  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                  </button>
-               </div>
-            </div>
+      <div class="animation-second-section">
+         <div class="container animation-second-section-wrapper">
+            <p class="animation-first-title">CONTEMOS TU HISTORIA</p>
+            <p class="animation-second-title">COMO NADIE MÁS LO HA HECHO...</p>
+            <p class="animation-first-text">Relátanos el qué, cómo, cuándo y dónde sobre tu negocio o proyecto. </p>
+            <p class="animation-second-text">Personalizamos la animación a tu necesidad para que comuniques de forma muy concreta y sencilla tus ideas más chidas e importantes. Afinaremos detalles, duración o urgencia para darte opciones accesibles, creativas e innovadoras que capten la atención de tu público.</p>
          </div>
+    
       </div>
-      <div class="design-second-section">
-         <img src="{{ asset('images/design/fondo_blanco.png') }}" alt="" class="design-second-section-fondo-blanco">
-         <img src="{{ asset('images/design/circulo.png') }}" class="design-second-section-burbuja">
-         <p class="design-second-section-text">CHOOSE WHAT YOU NEED</p>
-        <div class="design-second-section-row-wrapper">
-          <div class="position-relative design-second-section-row h-100">
-            <div class="owl-carousel owl-theme owl-carousel-section-2 h-100">
-              <div class="item text-center design-bottom-position">
-                <button class="design-second-section-col-button">A</button>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-              </div>
-              <div class="item text-center design-bottom-position">
-                <button class="design-second-section-col-button">B</button>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-              </div>
-              <div class="item text-center design-bottom-position">
-                <button class="design-second-section-col-button">C</button>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-                <p class="design-second-section-col-text">Ejemplo cualquiera</p>
-              </div>
-            </div>
-          </div>
+      <div class="animation-third-section">
+         <div class="owl-carousel owl-theme owl-carousel-animation">
+            <img alt="" src="{{ asset('images/animation/1.png') }}" class="animation-carrousel">
+            <img alt="" src="{{ asset('images/animation/2.png') }}" class="animation-carrousel">
+
+            <img alt="" src="{{ asset('images/animation/3.png') }}" class="animation-carrousel">
+
+            <img alt="" src="{{ asset('images/animation/4.png') }}" class="animation-carrousel">
+
+            <img alt="" src="{{ asset('images/animation/5.png') }}" class="animation-carrousel">
+
+            <img alt="" src="{{ asset('images/animation/6.png') }}" class="animation-carrousel">
+
+            <img alt="" src="{{ asset('images/animation/7.png') }}" class="animation-carrousel">
+
+            <img alt="" src="{{ asset('images/animation/8.png') }}" class="animation-carrousel">
+
+           <!-- <img alt="" src="{{ asset('images/animation/9.png') }}" class="animation-carrousel">-->
+
         </div>
-      </div>
-      <div class="design-third-section">
-         <div class="design-third-section-carousel">
-            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-               <div class="carousel-inner">
-                  <div class="carousel-item active">
-                     <img src="https://cdn.quasar.dev/img/mountains.jpg" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                     <img src="https://cdn.quasar.dev/img/parallax1.jpg" class="d-block w-100" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                     <img src="https://cdn.quasar.dev/img/parallax2.jpg" class="d-block w-100" alt="...">
-                  </div>
-               </div>
-               <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-               <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-               <span class="visually-hidden">Previous</span>
-               </button>
-               <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-               <span class="carousel-control-next-icon" aria-hidden="true"></span>
-               <span class="visually-hidden">Next</span>
-               </button>
-            </div>
-         </div>
-         <p class="design-third-section-text">EJEMPLOS GRÁFICOS</p>
       </div>
    </div>
       
