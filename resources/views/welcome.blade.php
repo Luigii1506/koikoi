30fe18315e6b63eeac3799cdb9858a3af82a7f3a
Luis Gerardo <luis.encinas1506@gmail.com>
2022-03-23 11:17:10 -0700

    Seccion Home en mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,10 +20,36 @@
             <p class="home-title-section-1">Si no te habían visto,</p>
             <p class="home-title-section-1"> es porque<span class="pink"> no nos habías visto.</span></p>
             <p class="home-title-section-2">El estudio creativo binacional que estabas buscando</p>
-          </div>  
+          </div>
+          <img class="home-desktop-mobile" src="{{ asset('images/computador_mobile.png') }}" alt="">
+          <div class="row home-second-section-mobile">
+            <div class="col-12 col-md-4">
+              <a href="{{ url('/web') }}">
+                <img alt="Quasar logo" src="{{ asset('images/inicio_card_1.png') }} " style="width: 100%; height: 100%" class="home-card-desktop">
+                <img alt="Quasar logo" src="{{ asset('images/inicio_card_1_mobile.png') }} " style="width: 100%; height: 100%" class="home-card-mobile">
+              </a>
+            </div>
+             <div class="col-12 col-md-4 mt-3">
+                <a href="{{ url('/design') }}">
+                  <img alt="Quasar logo" src="{{ asset('images/inicio_card_2.png') }}" style="width: 100%; height: 100%" class="home-card-desktop">
+                  <img alt="Quasar logo" src="{{ asset('images/inicio_card_2_mobile.png') }} " style="width: 100%; height: 100%" class="home-card-mobile">
+
+                </a>
+            </div>
+             <div class="col-12 col-md-4 mt-3">
+              <a href="{{ url('/animation') }}">
+                <img alt="Quasar logo" src="{{ asset('images/inicio_card_3.png') }}" style="width: 100%; height: 100%" class="home-card-desktop">
+                <img alt="Quasar logo" src="{{ asset('images/inicio_card_3_mobile.png') }} " style="width: 100%; height: 100%" class="home-card-mobile">
+              </a>
+            </div>
+            <div class="col-12">
+              <p class="third-section-title-mobile">¿CÓMO?</p> 
+            </div>
+          </div>
+           
       </div>
 
-      <div class="row">
+      <div class="row home-second-section-desktop">
         <div class="col-12 col-md-4">
           <a href="{{ url('/web') }}"><img alt="Quasar logo" src="{{ asset('images/inicio_card_1.png') }} " style="width: 100%; height: 100%" class="test"></a>
         </div>
@@ -37,9 +63,9 @@
       <div class="home-third-section">
         <div class="row home-row-third-section">
           <div class="col-12">
-            <div data-aos="fade-up"> <p class="home-third-section-title">How?</p></div>
+            <div data-aos="fade-up"> <p class="home-third-section-title">¿CÓMO?</p></div>
           </div>
-          <div class="col-12 col-sm-4 col-xl-3">
+          <div class="col-6 col-sm-4 col-xl-3">
             <img alt="Quasar logo" src="{{ asset('images/tooltip_1.png') }}" class="home-third-section-small-img">
             <p class="home-third-section-text-2">¿QUÉ NECESITAS?</p>
             <p class="home-third-section-text">Tenemos opciones que</p>
@@ -47,13 +73,13 @@
             <p class="home-third-section-text">IDENTIDAD VISUAL de</p>
             <p class="home-third-section-text">tu proyecto o negocio.</p>
           </div>  
-          <div class="col-12 col-sm-4 col-xl-3">
+          <div class="col-6 col-sm-4 col-xl-3">
             <img alt="Quasar logo" src="{{ asset('images/tooltip_2.png') }}" class="home-third-section-small-img">
             <p class="home-third-section-text-2">¡CONTÁCTANOS!</p>
             <p class="home-third-section-text">Whatsapp, mensaje o</p>
             <p class="home-third-section-text">paloma mensajera.</p>
           </div>    
-          <div class="col-12 col-sm-4 col-xl-3">
+          <div class="col-6 col-sm-4 col-xl-3">
             <img alt="Quasar logo" src="{{ asset('images/tooltip_3.png') }}" class="home-third-section-small-img ">
             <p class="home-third-section-text-2">QUEREMOS CONOCERTE</p>
             <p class="home-third-section-text">Cuéntanos tu proyecto.</p>
@@ -62,7 +88,7 @@
             <p class="home-third-section-text">La idea es cotizarte a la</p>
             <p class="home-third-section-text">medida.</p>
           </div>
-          <div class="col-12 col-xl-3">   
+          <div class="col-6 col-xl-3">   
             <img alt="Quasar logo" src="{{ asset('images/tooltip_4.png') }}" class="home-third-section-big-img">
             <p class="home-third-section-text-2">¡TRABAJAMOS JUNTOS</p>
             <p class="home-third-section-text-2">TODO EL PROCESO!</p>
@@ -73,6 +99,8 @@
         </div>
         <div class="row q-mt-xl button-welcome">
           <img alt="Quasar logo" src="{{ asset('images/details.png') }}" class="home-third-section-details-img">
+          <img alt="Quasar logo" src="{{ asset('images/details_mobile.png') }}" class="home-third-section-details-img-mobile">
+
         </div>
         <div class="row home-third-section-circulo-img">
           <div 
@@ -87,6 +115,7 @@
               <p class="home-postick-text-2">koikoi@studio.com</p>
             </div>
           </div>
+          <img alt="Quasar logo" src="{{ asset('images/footer_mobile.png') }}" class="footer-img-mobile">
         </div>
         <div class="footer">
           <img alt="Quasar logo" src="{{ asset('images/footer.png') }}" class="footer-img">
