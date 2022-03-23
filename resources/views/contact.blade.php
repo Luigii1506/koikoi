ffec74d9ce4127efd408ab142d7d00b41e70987a
Luis Gerardo <luis.encinas1506@gmail.com>
2022-03-23 11:52:44 -0700

    Contact us mobile

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -16,24 +16,26 @@
              </div>
          </div>
       <img alt="koikoi_logo" src="{{ asset('images/contact/fondo_contacto.png') }}" class="contacto-fondo-pantalla">
+      <img alt="koikoi_logo" src="{{ asset('images/contact/fondo_contacto_mobile.png') }}" class="contacto-fondo-pantalla_mobile">
       <!--
       <div class="header">
             <img alt="koikoi_logo" src="{{ asset('images/koikoi_logo_blanco.png') }}" class="logo">
       </div>
             -->
       <div class="contact-wrapper">
-            <div class="contact-title-wrappaer"><p class="contact-title">CONTACT us</p></div>
+            <div class="contact-title-wrappaer"><p class="contact-title">CONTÁCTANOS</p></div>
             <div class="contact-form-wrapper">
                   <form class="custom-form" method="post" action="{{ url('/sendmail') }}">
                         {{  csrf_field()   }}
-                        <div class="form-group">
-                              <span class="contact-span"><span class="contact-span">Send us a mail</span></span>
+                        <div class="form-group mb-3">
+                              <span class="contact-span"><span class="contact-span">Mándanos un correo</span></span>
                               <textarea class="form-control textarea custom-form-textarea" rows="5" placeholder="Hey me interesa una cotización" name="mensaje"></textarea>
                         </div>
                         <div class="form-group">
-                              <span class="contact-span">Your mail</span>
+                              <span class="contact-span">Tu correo</span>
                               <input type="email" class="form-control custom-form-input"  placeholder="correo@gmail.com" name="email">
                         </div>
+                        <button class="btn btn-contact">ENVIAR</button>
                   </form>
             </div>
       </div>
