c95114d24fde27c5a3854ed8826d870895f7e0bf
Luis Gerardo <luis.encinas1506@gmail.com>
2022-04-08 06:18:59 -0700

    Codigo de sección web

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
    </head>
    <body class="antialiased">
       <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: black">
-         <div class="container-fluid">
+         <div class="container-fluid"> 
             <a class="navbar-brand" href="#"><img alt="koikoi_logo" src="{{ asset('images/koikoi_logo_blanco.png') }}" class="logo-cel"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
